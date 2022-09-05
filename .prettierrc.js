86ad218c66ffb79d2dc8addb63f2d47407b6d960
Gizem Izmir <gizkes1996@gmail.com>
2022-09-05 12:19:54 +0300

    prettier-plugin-sort-imports added

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,13 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 80,
+  importOrder: [
+    '^(react|react-native)$',
+    '<THIRD_PARTY_MODULES>',
+    '^@server/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
