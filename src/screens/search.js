256f20021b2e738eec0f8734bf9f1d4ee235ab75
Gizem Izmir <gizkes1996@gmail.com>
2022-09-06 12:26:40 +0300

    Search screen created.

---
+++ src/screens/search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Search = () => {
+  return (
+    <View style={styles.searchContainer}>
+      <Text>Search Page</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    backgroundColor: '#FFF',
+  },
+});
+
+export default Search;
