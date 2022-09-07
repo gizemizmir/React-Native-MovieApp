634cb059554983a809cc8cec90045c563a378bab
Gizem Izmir <gizkes1996@gmail.com>
2022-09-07 11:10:40 +0300

    Home and Search screen updated and axios used

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,10 +1,79 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+
+import axios from 'axios';
+import {ScrollView} from 'react-native-gesture-handler';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+
+import MovieItem from '../components/MovieItem';
 
 const Home = () => {
+  const [movies, SetMovies] = useState([]);
+
+  const handleGetMovies = movieGenre => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${movieGenre}?api_key=aea92b0c171765ec9ae69fdf13f31a39&language=en-US&page=1`,
+      )
+      .then(response => {
+        SetMovies(response.data.results);
+      });
+  };
+
+  useEffect(() => {
+    handleGetMovies('top_rated');
+  }, []);
+
+  const renderTweetSeparatorItem = ({item}) => {
+    return (
+      <View
+        style={[
+          styles.separator,
+          {
+            backgroundColor: '#ccc',
+          },
+        ]}
+      />
+    );
+  };
+
+  const renderMovieItem = ({item}) => {
+    return <MovieItem movie={item} />;
+  };
+
   return (
     <View style={styles.homeContainer}>
-      <Text onPress={{}}>Home Page</Text>
+      <ScrollView
+        style={styles.filterView}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}>
+        <Pressable
+          style={styles.filterButton}
+          onPress={() => handleGetMovies('top_rated')}>
+          <Text style={styles.filterText}>Top Rated</Text>
+        </Pressable>
+        <Pressable
+          style={styles.filterButton}
+          onPress={() => handleGetMovies('now_playing')}>
+          <Text style={styles.filterText}>Latest</Text>
+        </Pressable>
+        <Pressable
+          style={styles.filterButton}
+          onPress={() => handleGetMovies('popular')}>
+          <Text style={styles.filterText}>Popular</Text>
+        </Pressable>
+        <Pressable
+          style={styles.filterButton}
+          onPress={() => handleGetMovies('upcoming')}>
+          <Text style={styles.filterText}>Upcoming</Text>
+        </Pressable>
+      </ScrollView>
+      <FlatList
+        data={movies}
+        renderItem={renderMovieItem}
+        keyExtractor={(item, index) => `movie-${item.id}`}
+        ItemSeparatorComponent={renderTweetSeparatorItem}
+      />
     </View>
   );
 };
@@ -12,6 +81,23 @@ const Home = () => {
 const styles = StyleSheet.create({
   homeContainer: {
     backgroundColor: '#FFF',
+    height: '100%',
+  },
+  filterView: {
+    paddingHorizontal: 10,
+  },
+  separator: {
+    width: '100%',
+    height: 1,
+  },
+  filterButton: {
+    padding: 10,
+    backgroundColor: '#ececec',
+    margin: 10,
+    borderRadius: 5,
+  },
+  filterText: {
+    padding: 5,
   },
 });
 
--- src/screens/search.js
+++ src/screens/search.js
@@ -1,10 +1,96 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Alert,
+  FlatList,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+import axios from 'axios';
+import IonIcons from 'react-native-vector-icons/dist/Ionicons';
+
+import MovieItem from '../components/MovieItem';
 
 const Search = () => {
+  const [searchMovies, SetSearchMovies] = useState(null);
+  const [searchText, SetSearchText] = useState(null);
+
+  const handleGetMovies = () => {
+    SetSearchMovies(null);
+    if (searchText != '') {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=aea92b0c171765ec9ae69fdf13f31a39&language=en-US&page=1&query=${searchText}`,
+        )
+        .then(response => {
+          SetSearchMovies(response.data);
+          SetSearchText('');
+        });
+    } else {
+      Alert.alert('Alert', 'Type anything for search');
+    }
+  };
+
+  const renderTweetSeparatorItem = ({item}) => {
+    return (
+      <View
+        style={[
+          styles.separator,
+          {
+            backgroundColor: '#ccc',
+          },
+        ]}
+      />
+    );
+  };
+
+  const renderMovieItem = ({item}) => {
+    return <MovieItem movie={item} />;
+  };
+
+  useEffect(() => {
+    console.log('searched');
+  }, [searchMovies]);
+
   return (
     <View style={styles.searchContainer}>
-      <Text>Search Page</Text>
+      <View style={styles.bottomArea}>
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          clearButtonMode="always"
+          value={searchText}
+          onChangeText={text => {
+            SetSearchText(text);
+          }}
+          onSubmitEditing={() => handleGetMovies()}
+        />
+        <Pressable onPress={() => handleGetMovies()}>
+          <IonIcons
+            style={styles.bottomAreaIcon}
+            name="search"
+            size={25}
+            color="#2385E1"
+          />
+        </Pressable>
+      </View>
+      {searchMovies ? (
+        searchMovies.total_results > 0 ? (
+          <FlatList
+            data={searchMovies.results}
+            renderItem={renderMovieItem}
+            keyExtractor={(item, index) => `movie-${item.id}`}
+            ItemSeparatorComponent={renderTweetSeparatorItem}
+          />
+        ) : (
+          <Text style={styles.searchPageText}>Couldn't Find Any Movie</Text>
+        )
+      ) : (
+        <Text style={styles.searchPageText}>Search Movie</Text>
+      )}
     </View>
   );
 };
@@ -12,6 +98,38 @@ const Search = () => {
 const styles = StyleSheet.create({
   searchContainer: {
     backgroundColor: '#FFF',
+    height: '100%',
+  },
+  bottomArea: {
+    height: 80,
+    paddingLeft: 20,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  input: {
+    margin: 15,
+    marginLeft: 0,
+    height: 30,
+    width: '80%',
+    borderColor: '#c4c4c4',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 5,
+  },
+  bottomAreaIcon: {
+    marginTop: 15,
+    marginLeft: 8,
+  },
+  separator: {
+    width: '100%',
+    height: 1,
+  },
+  searchPageText: {
+    display: 'flex',
+    alignSelf: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: '50%',
   },
 });
 
