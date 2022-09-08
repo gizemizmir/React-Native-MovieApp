d9189d74cff4a741bb3bdc9967a9f0188fca625d
Gizem Izmir <gizkes1996@gmail.com>
2022-09-08 11:33:18 +0300

    ProfileSettings screen created

---
+++ src/screens/profileSettings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const ProfileSettings = () => {
+  return (
+    <View>
+      <Text>ProfileSettings</Text>
+    </View>
+  );
+};
+
+export default ProfileSettings;
